7516822cd5adcf8cf950c277d64d543326531b2a
aakashbentray <aakash@bentraytech.com>
2025-03-18 22:02:41 +0545

    create custom card for myorders

--- lib/view/dashboard_screens/home/components/my_orders_list/my_orders_list.dart
+++ lib/view/dashboard_screens/home/components/my_orders_list/my_orders_list.dart
@@ -14,57 +14,68 @@ class MyOrders extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(
-          width: MediaQuery.sizeOf(context).width,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.r),
-                border:
-                    Border.all(color: CleanerAppcolors.primaryminigreycolor)),
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 22.h, horizontal: 10.w),
-              child: Column(
+        MyOrdersCard()
+      ],
+    );
+  }
+}
+
+class MyOrdersCard extends StatelessWidget {
+  const MyOrdersCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.r),
+            border:
+                Border.all(color: CleanerAppcolors.primaryminigreycolor)),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 22.h, horizontal: 10.w),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Lautinez Martinez',
-                            style: listiletitlefont,
-                          ),
-                          SizedBox(
-                            height: 5.h,
-                          ),
-                          Text(
-                            'lautinazo@gmail.com',
-                            style: splashloadingfond,
-                          ),
-                        ],
+                      Text(
+                        'Lautinez Martinez',
+                        style: listiletitlefont,
                       ),
-                      CleanerChip(
-                        onPressed: () {
-                          showModalBottomSheet(
-                            showDragHandle: true,
-                            context: context,
-                            builder: (context) {
-                              return CleanerAppBottomSheet();
-                            },
-                          );
-                        },
-                        label: 'View',
-                        backgroundColor: CleanerAppcolors.primarybrowncolor,
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      Text(
+                        'lautinazo@gmail.com',
+                        style: splashloadingfond,
                       ),
                     ],
-                  )
+                  ),
+                  CleanerChip(
+                    onPressed: () {
+                      showModalBottomSheet(
+                        showDragHandle: true,
+                        context: context,
+                        builder: (context) {
+                          return CleanerAppBottomSheet();
+                        },
+                      );
+                    },
+                    label: 'View',
+                    backgroundColor: CleanerAppcolors.primarybrowncolor,
+                  ),
                 ],
-              ),
-            ),
+              )
+            ],
           ),
-        )
-      ],
+        ),
+      ),
     );
   }
 }
