762b2837db26b86160bbe7fa38e1e59578a5c993
akash2058 <aakashsharma73235@gmail.com>
2025-04-13 13:36:57 +0545

    update new urls

--- lib/utils/apiurl.dart
+++ lib/utils/apiurl.dart
@@ -1,6 +1,6 @@
 class AppUrl {
   static const mainUrl =
-      'https://5212-2404-7c00-44-fb17-85dc-1ca8-f9e1-c0cc.ngrok-free.app/api/';
+      'https://2ecc-2404-7c00-44-9711-74a0-39b1-6db1-d953.ngrok-free.app/api/';
   // static const mainUrl = 'http://127.0.0.1:8000/api/';
   static const loginurl = '${mainUrl}login';
   static const binbooking = '${mainUrl}bin-bookings';
